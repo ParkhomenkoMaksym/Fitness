666388fe2eb68fa0483bc950007db6c9d2820936
Parkhomenko Maksym <maxice777163@gmail.com>
2024-03-21 15:00:46 +0000

    Add project files.

---
+++ CodeBlogFitnessBL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitnessBL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Create a new gender.
+        /// </summary>
+        /// <param name="name">Name of gender</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrEmpty(name)) 
+                throw new ArgumentNullException("Can't be empty", nameof(name));
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ CodeBlogFitnessBL/Model/User.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeBlogFitnessBL.Model
+{
+    
+    public class User
+    {
+        #region Properties
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gender.
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// Birthday.
+        /// </summary>
+        public DateTime BirthDate { get; }
+
+        /// <summary>
+        /// Weight.
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Height
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+        /// <summary>
+        /// Creat a new user
+        /// </summary>
+        /// <param name="name">Name. </param>
+        /// <param name="gender">Gender. </param>
+        /// <param name="birthDate">Birthday. </param>
+        /// <param name="weight">Weight. </param>
+        /// <param name="height">Height. </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            #region Exceptions
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("The name can't be null");
+                
+            }
+            if(gender == null) 
+            {
+                    throw new ArgumentNullException("Gender can't be null", nameof(gender));
+            
+            }
+            if(birthDate < DateTime.Parse("01.01.1990") || birthDate >= DateTime.Now) 
+            {
+                    throw new ArgumentNullException("Invalid a date of birthday", nameof(birthDate));
+            
+            }
+            if(weight <= 0) 
+            {
+                    throw new ArgumentNullException("Weight can't be less or equals to zero.", nameof(weight));
+            
+            }
+            if(height <= 0) 
+            {
+                    throw new ArgumentNullException("Height can't be less or equals to zero.", nameof(height));
+            
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
